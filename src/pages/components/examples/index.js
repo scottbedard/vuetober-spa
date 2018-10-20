377908da47c404e1dc092b9008d064a5edf1711e
Scott Bedard <scottbedard90@gmail.com>
2018-10-20 15:34:11 -0700

    fix grid example key

--- src/pages/components/examples/index.js
+++ src/pages/components/examples/index.js
@@ -5,6 +5,6 @@ export default {
     '<v-button>': () => import('./button_example.vue'),
     '<v-collapse-transition>': () => import('./collapse_transition_example.vue'),
     '<v-fade-transition>': () => import('./fade_transition_example.vue'),
-    '<v-grid>': () => import('./grid_example.vue'),
+    '<v-grid> & <v-grid-cell>': () => import('./grid_example.vue'),
     '<v-input>': () => import('./input_example.vue'),
 };
