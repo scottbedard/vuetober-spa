fb1666865f7791bf246ca333589782501c9e3224
Scott Bedard <scottbedard90@gmail.com>
2018-10-21 15:56:05 -0700

    testing deployments

--- src/layouts/default/footer/footer.vue
+++ src/layouts/default/footer/footer.vue
@@ -1,6 +1,7 @@
 <template>
     <footer class="py-4">
-        <v-margin padded>@todo: create a basic responsive footer
+        <v-margin padded>
+            Hello from the footer
         </v-margin>
     </footer>
 </template>
