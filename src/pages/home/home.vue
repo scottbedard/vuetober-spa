39f5bb51daddc0192535e9f4a1bd7dfe4684c3eb
Scott Bedard <scottbedard90@gmail.com>
2018-10-21 21:51:44 -0700

    center home page text

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-margin padded>
+    <v-margin class="text-center py-20" padded>
         <h1 class="font-light mb-4">Welcome to your new Vuetober SPA</h1>
         <h2 class="font-light">It's time to build something amazing!</h2>
     </v-margin>
